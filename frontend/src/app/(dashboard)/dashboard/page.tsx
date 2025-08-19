31790b8058458bb2b51270a5defce7366a7758c5
Aman Bhardwaj <amanbhardwaj5931@gmail.com>
2025-08-19 18:07:25 +0530

    Added stats cards

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import ChartCard from "@/components/ChartCard";
+import Topbar from "@/components/TopBar";
+
 type StatCardProps = {
   icon: React.ReactNode;
   value: string;
@@ -6,10 +11,18 @@ type StatCardProps = {
   deltaClass?: string; // <-- make it optional
 };
 
-export default function StatCard({ icon, value, label, delta, deltaClass }: StatCardProps) {
+export default function StatCard({ icon, value, label, delta, deltaClass, }: StatCardProps) {
   return (
     <div className="bg-slate-800/50 rounded-2xl p-5 shadow-lg">
-      <h1>Personal Dahsboard</h1>
+      <Topbar />
+     
+      <div className={`mt-3 text-sm ${deltaClass ?? "text-green-400"}`}>
+        {delta}
+      </div>
+       <div className="flex items-start justify-between gap-4">
+        <ChartCard title="Payments Overview" />
+      </div>
     </div>
+    
   );
 }
